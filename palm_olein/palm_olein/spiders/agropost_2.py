26da8744c901f614640f1051df0cdf831fca0d77
Weicong Kong <weicong.kong@gmail.com>
2022-06-29 21:13:17 +1000

    live session and debugging, advise on a better practice to prepare to raw scraped data from scrapy. more learning on the following will be needed: - relying XPATH to grab the data of interest - finding some anchor element and extract the data from those anchor points will be more robust

--- palm_olein/palm_olein/spiders/agropost_2.py
+++ palm_olein/palm_olein/spiders/agropost_2.py
@@ -6,6 +6,9 @@ import pandas as pd
 
 class AgropostSpider(scrapy.Spider):
     name = "agropost_2"
+
+    # WK: as we have seen together, the pattern of paging is straightforward. We can keep exploring until will hit a
+    #   page that shown Error 404. This logic can help you build a finite list of urls to feed here to the scrapy.Spider
     start_urls = [
         "https://agropost.wordpress.com/",
         "https://agropost.wordpress.com/page/2/",
@@ -25,11 +28,12 @@ class AgropostSpider(scrapy.Spider):
         for date in response.css('h2'):
             yield {
                 'date': date.css('a::text').get(),  # fix_me: this works but its not robust
+                'price': date.xpath('//div/table[2]/tbody/tr/td')[8].get()
             }
-        for price in response.css('table'):
-            yield {
-                "price": price.css('td::text')[2].get()  # fix_me: this works but its not robust
-            }
+        # for price in response.css('table'):
+        #     yield {
+        #         "price": price.css('td::text')[2].get()  # fix_me: this works but its not robust
+        #     }
 
         # next_page = response.css('li.next a::attr(href)').get()
         # if next_page is not None:
