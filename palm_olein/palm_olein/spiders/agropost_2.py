27dfc2883438bcfbe497ce272b03279e53e3e053
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2022-07-01 09:10:03 +1000

    wilson's work on the xpath for the scraper has worked immaculately. I'm very impressed. still a long list of things to be done to make this scraper work as i want it to.

--- palm_olein/palm_olein/spiders/agropost_2.py
+++ palm_olein/palm_olein/spiders/agropost_2.py
@@ -1,7 +1,4 @@
 import scrapy
-import pandas as pd
-
-# from selenium_and_firefox import date_list
 
 
 class AgropostSpider(scrapy.Spider):
@@ -20,7 +17,7 @@ class AgropostSpider(scrapy.Spider):
         "https://agropost.wordpress.com/page/8/",
         "https://agropost.wordpress.com/page/9/",
         "https://agropost.wordpress.com/page/10/"
-        # fix_me: use a while loop to append a new number to the url until i get a 404 then termiate the loop
+        # fix_me: use a while loop to append a new number to the url until i get a 404 then terminate the loop
     ]
 
     def parse(self, response):
@@ -31,11 +28,13 @@ class AgropostSpider(scrapy.Spider):
 
             # WK: exercise for you Michael, you can try to identify the date_text being invalid to skip an iteration,
             #   so that you don't need to clean this at a later stage
+
             midday_closing = title_text_raw.split('\xa0')[-1]
+
             # WK: XPATH - get the next following sibling where the tag is "div",
             #   then drill down to the 2nd table, 2nd row and 3rd cell
-            palm_olein_price = h2.xpath(".//following-sibling::div[1]/table[2]/tbody/tr[2]/td[3]//text()").get()
 
+            palm_olein_price = h2.xpath(".//following-sibling::div[1]/table[2]/tbody/tr[2]/td[3]//text()").get()
 
             yield {
                 'date': date_text,
@@ -43,6 +42,11 @@ class AgropostSpider(scrapy.Spider):
                 'price': palm_olein_price
             }
 
-    # fix_me: need to parse better. better to get it right early in the process rather than later
+            next_page = response.css('li.next a::attr(href)').get()
+            # todo: this is a placeholder. need to get the xpath working properly
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback=self.parse)
 
 
+# scrapy crawl agropost_2 -O today.json
