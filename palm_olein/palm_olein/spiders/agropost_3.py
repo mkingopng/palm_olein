68e09486496fed28f9c3306f0052858f30ab7643
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2022-07-07 21:26:38 +1000

    I've made another change to the price part of the parse function. It should now only accept numbers of 3-4 digits and no more than 2 decimal places. This has resulted in a large decrease the total number of rows to 3430. a quick visual inspection indicates that it looks ok but as repeated in many previous commits, i'd like ot have a more objective, quantifiable way to determining the data is indeed complete and correct
    it seems to be working.
    
    there are some odd bugs. I notice that the dates around rows 213 - 228 look wrong. It's as if the scraper has ignored the xpaths and split for those lines. There are also some other odd characters that have slipped through. strange.
    
    it is very interesting to watch the feed on the terminal pane as it scrapes. I wonder whether it is possible to record this as a log file like we've done in the past for ML training?

--- palm_olein/palm_olein/spiders/agropost_3.py
+++ palm_olein/palm_olein/spiders/agropost_3.py
@@ -17,16 +17,21 @@ class AgropostSpider(scrapy.Spider):
             yield scrapy.Request(url=f"https://agropost.wordpress.com/page/{idx}/", callback=self.parse)
 
     def parse(self, response):
-        pattern = re.compile("[0-9]")
+        pattern_1 = re.compile("[0-9]")
+        pattern_2 = re.compile("^\d\d+(\.[1-9])?$")  # ("[0-9]{3,4}]")
         for h2 in response.xpath("//h2[@class='entry-title']"):
-            if h2.xpath("./a[1]//text()").get() is not None and pattern.match(h2.xpath("./a[1]//text()").get()):
+            if h2.xpath("./a[1]//text()").get() is not None and pattern_1.match(h2.xpath("./a[1]//text()").get()):
                 title_text_raw = h2.xpath("./a[1]//text()").get()
                 date_text = title_text_raw.split(' – ')[0]
                 midday_closing = title_text_raw.split('\xa0')[-1]
             else:
                 pass
-            if h2.xpath(".//following-sibling::div[1]/table[2]/tbody/tr[2]/td[3]//text()").get() is not None:
+            if pattern_2.match(h2.xpath(".//following-sibling::div[1]/table[2]/tbody/tr[2]/td[3]//text()").get()):
                 palm_olein_price = h2.xpath(".//following-sibling::div[1]/table[2]/tbody/tr[2]/td[3]//text()").get()
+                # h2.xpath(".//following-sibling::div[1]/table[2]/tbody/tr[2]/td[3]//text()").get() is not None and\
+            else:
+                pass
+
             yield {
                 'date': date_text,
                 'updated_at': midday_closing,
@@ -37,4 +42,4 @@ class AgropostSpider(scrapy.Spider):
             # item["midday_closing"] = midday_closing
             # item["price"] = palm_olein_price
 
-# scrapy crawl agropost_3 -O agp4.csv
+# scrapy crawl agropost_3 -O agp5.csv
