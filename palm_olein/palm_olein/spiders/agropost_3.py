454f5134748e65bcc4a5c435b84f6035da181277
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2022-07-10 18:31:01 +1000

    It feels like a good time to branch.
    the scraper now returns EVERYTHING. This is quite a different design philosophy to what we were chasing earlier, but i think it makes sense.
    
    the scraper should take everything we might need from the website.
    
    the pipelines should take care of cleaning then posting to db.

--- palm_olein/palm_olein/spiders/agropost_3.py
+++ palm_olein/palm_olein/spiders/agropost_3.py
@@ -19,10 +19,9 @@ class AgropostSpider(scrapy.Spider):
     def parse(self, response):
         pattern_1 = re.compile("[0-9]")
         datetime_pattern = re.compile(r'\d{1,2}\w{0,2} \w+ \d{2,4}')
-        pattern_2 = re.compile(r"^\d\d+(\.[1-9])?$")  # ("[0-9]{3,4}]")
+        pattern_2 = re.compile(r"^\d\d+(\.[1-9])?$")
         for h2 in response.xpath("//h2[@class='entry-title']"):
             title_text_raw = h2.xpath("./a[1]//text()").get()
-            # date_text = title_text_raw.split(' – ')[0]
             extracted = datetime_pattern.search(title_text_raw)
             date_text = extracted.group() if extracted else None
             midday_closing = title_text_raw.split('\xa0')[-1]
@@ -31,20 +30,6 @@ class AgropostSpider(scrapy.Spider):
             product = second_row.xpath(".//td[1]//text()").get()
             buy_or_sell = second_row.xpath(".//td[2]//text()").get()
             price = second_row.xpath(".//td[3]//text()").get()
-
-            # WK: don't do the condition because we want to keep the raw form as much as we can
-            # if h2.xpath("./a[1]//text()").get() is not None and pattern_1.match(h2.xpath("./a[1]//text()").get()):
-            #     title_text_raw = h2.xpath("./a[1]//text()").get()
-            #     # date_text = title_text_raw.split(' – ')[0]
-            #     extracted = datetime_pattern.search(title_text_raw)
-            #     date_text = extracted.group() if extracted else title_text_raw
-            #     midday_closing = title_text_raw.split('\xa0')[-1]
-            # else:
-            #     pass
-            # if pattern_2.match(h2.xpath(".//following-sibling::div[1]/table[2]/tbody/tr[2]/td[3]//text()").get()):
-            #     palm_olein_price = h2.xpath(".//following-sibling::div[1]/table[2]/tbody/tr[2]/td[3]//text()").get()
-            # else:
-            #     pass
 
             # WK: we include 'raw_title', 'buy_or_sell', 'url' to help us clean the data later.
             #   you can include as many fields as possible if they may be of any potential help
@@ -57,9 +42,5 @@ class AgropostSpider(scrapy.Spider):
                 'buy_or_sell': buy_or_sell,
                 'price': price
             }
-            # item = PalmOleinItem()
-            # item["date"] = date_text
-            # item["midday_closing"] = midday_closing
-            # item["price"] = palm_olein_price
 
 # scrapy crawl agropost_3 -O agp5.csv
